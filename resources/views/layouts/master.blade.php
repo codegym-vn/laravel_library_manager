dc40d5b55916f7bad7be49b0b33c66ddb93ab584
tu <nguyen.tu.st73@gmail.com>
2018-10-19 14:57:09 +0700

    CRUD category

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,6 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css"
+          integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
 
 </head>
 <style type="text/css">
@@ -24,76 +26,40 @@
             <img src="{{ asset('image1/logo1.png') }}" alt="logo" style="width:250px;">
         </a>
         <!-- Links -->
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link 1</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link 2</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link 3</a>
-            </li>
-        </ul>
-        <form class="form-inline my-2 my-lg-0" id="search1"  action="#">
+        {{--<ul class="navbar-nav">--}}
+        {{--<li class="nav-item">--}}
+        {{--<a class="nav-link" href="#">Link 1</a>--}}
+        {{--</li>--}}
+        {{--<li class="nav-item">--}}
+        {{--<a class="nav-link" href="#">Link 2</a>--}}
+        {{--</li>--}}
+        {{--<li class="nav-item">--}}
+        {{--<a class="nav-link" href="#">Link 3</a>--}}
+        {{--</li>--}}
+        {{--</ul>--}}
+        <form class="form-inline my-2 my-lg-0" id="search1" action="#">
             <input class="form-control mr-sm-2" type="text" placeholder="Search">
             <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
         </form>
     </nav>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-2 bg-secondary" style="height: 900px">
+            <div class="col-md-2 bg-secondary" style="height: 900px">
                 <hr>
                 <div class="btn-group">
-                    <button type="button" id="button1" class="btn btn-light">Quản lý sách</button>
-                    <button class="btn btn-light dropdown-toggle" data-toggle="dropdown">
+                    <button type="button" class="btn btn-light dropdown-toggle" data-toggle="dropdown">
+                        <a id="button1" class="btn bg-light">Quản lý các loại sách</a>
                         <span class="caret"></span>
                     </button>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">Add</a>
-                        <a class="dropdown-item" href="#">Edit</a>
-                        <a class="dropdown-item" href="#">Delete</a>
-                    </div>
-                </div>
-                <hr>
-                <div class="btn-group">
-                    <button type="button" id="button1" class="btn btn-light">Quản lý tác giả</button>
-                    <button class="btn btn-light dropdown-toggle" data-toggle="dropdown">
-                        <span class="caret"></span>
-                    </button>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">Tablet</a>
-                        <a class="dropdown-item" href="#">Smartphone</a>
-                    </div>
-                </div>
-                <hr>
-                <div class="btn-group">
-                    <button type="button" id="button1" class="btn btn-light">Quản lý các loại sách</button>
-                    <button class="btn btn-light dropdown-toggle" data-toggle="dropdown">
-                        <span class="caret"></span>
-                    </button>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">Tablet</a>
-                        <a class="dropdown-item" href="#">Smartphone</a>
-                    </div>
-                </div>
-                <hr>
-                <div class="btn-group">
-                    <button type="button" id="button1" class="btn btn-light">Quản lý sách</button>
-                    <button class="btn btn-light dropdown-toggle" data-toggle="dropdown">
-                        <span class="caret"></span>
-                    </button>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">Tablet</a>
-                        <a class="dropdown-item" href="#">Smartphone</a>
+                    <div class="dropdown-menu" style="width: 280px">
+                        <a class="dropdown-item" href="{{ route('categories_index') }}">List Categories</a>
+                        <a class="dropdown-item" href="{{ route('category_create') }}">Create</a>
                     </div>
                 </div>
                 <hr>
             </div>
-            <div class="col-10 ">
-                <div class="container-fluid">
-                    @yield('content')
-                </div>
+            <div class="col-md-10 ">
+                @yield('content')
             </div>
         </div>
     </div>
